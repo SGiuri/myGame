135f059222cfd9eb33a3dcd61aa9284c1aa25732
SGiuri <s.giuri@issm.it>
2020-11-11 12:17:37 +0100

    Ship Placed

--- main.py
+++ main.py
@@ -24,6 +24,26 @@ YELLOW_LASER = pygame.image.load(assets_location.join("assets", 'pixel_laser_yel
 YELLOW_SHIP = pygame.image.load(assets_location.join("assets", 'pixel_ship_yellow.png'))
 
 
+class Ship:
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img, self.laser_img = None, None
+        self.laser = []
+        self.cool_down_counter = 0
+
+    def draw(self, window):
+        window.blit(self.ship_img, (self.x, self.y))
+
+class Player(Ship):
+    def __init__(self,x,y,health=100):
+        super().__init__(x,y,health)
+        self.ship_img = YELLOW_SHIP
+        self.ship_laser = YELLOW_LASER
+
+
+
 def main():
     run = True
     FPS = 60
@@ -34,24 +54,31 @@ def main():
 
     clock = pygame.time.Clock()
 
-
     def redraw_window():
-        WIN.blit(BG, (0,0))
-        level_label = main_font.render(f"Level = {level}", 1, (255,255,255))
-        lives_label = main_font.render(f"Lives = {lives}", 1, (255, 255, 255))
+        WIN.blit(BG, (0, 0))
+        level_label = main_font.render(f"Level = {level}", True, (255, 255, 255))
+        lives_label = main_font.render(f"Lives = {lives}", True, (255, 255, 255))
 
-        WIN.blit(lives_label, (10,10))
-        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10,10))
+        WIN.blit(lives_label, (10, 10))
+        WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+
+        player = Player(100, 100)
+        player.draw(WIN)
+
         pygame.display.update()
-
 
     while run:
         clock.tick(FPS)
         redraw_window()
 
+
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+
+
 
 
 main()
